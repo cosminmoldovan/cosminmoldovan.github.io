12bc4aea6c0903645364c88ea926902a93109fdb
Cosmin Moldovan <cosmin.moldovan@protonmail.com>
2022-09-18 11:49:03 +0300

    deleted extra files

--- src/App.js
+++ src/App.js
@@ -1,48 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import LoadingScreen from './modules/LoadingScreen';
 import Navbar from './modules/Navbar';
 import Hero from './modules/Hero';
-import About from './modules/About';
 import ProjectsData from './ProjectsData';
 import Card from './modules/Card';
 import './css/main.css';
-import { ReactComponent as FolderIcon } from './images/folder.svg'
-
-
 
 function App() {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 3000);
-  }, [])
-
   const projects = ProjectsData.map(project => (
     <Card {...project} />
   ))
   return (
-    <>
-      {loading === false ? (
         <div className="App">
           <Navbar />
           <Hero />
-          
+
           <section className='projects' id='projects'>
-            {/* <div className='projects__title'><FolderIcon /> Selected Projects</div> */}
             {projects}
           </section>
 
-          <section className='secondary-projects'>
-
-          </section>
-
-          <About />
-          
         </div>
-      ) : (
-        <LoadingScreen />
-      )}
-    </>
   );
 }
 
