3b9163f84dfd30675b23f533baa11bc7c1626220
Cosmin Moldovan <cosminmol@proton.me>
2022-11-19 09:44:52 +0200

    delete github link

--- src/modules/Navbar.js
+++ src/modules/Navbar.js
@@ -37,7 +37,6 @@ function Navbar() {
                 <ul ref={menu} className="nav__list">
                     <li className="nav__item"><a className="nav__link" href="#projects">Projects</a></li>
                     <li className="nav__item"><a className="nav__link" href="https://resilient-temper-595.notion.site/Hi-I-m-Cosmin-dc007903a27d4ad98da581173d443352" target="_blank">About</a></li>
-                    {/* <li className="nav__item"><a className="nav__link" href="https://github.com/cosminmoldovan" target="_blank">GitHub</a></li> */}
                     <li className="nav__item"><a className="nav__link" href="./Cosmin_Moldovan_Resume.pdf" target="_blank">Resume</a></li>
                     <li className="nav__item"><a className="nav__link" href="https://www.linkedin.com/in/cosmin-moldovan/" target="_blank">LinkedIn</a></li>
                 </ul>
